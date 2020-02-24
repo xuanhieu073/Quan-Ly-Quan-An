fe5a20f844ec5b9961402b8b2decf1e5310b4d5f
xuanhieu073 <xuanhieu073@gmail.com>
2020-02-24 18:07:53 +0700

    product edit layout

--- front-end/src/components/FoodEdit.vue
+++ front-end/src/components/FoodEdit.vue
@@ -9,7 +9,9 @@
     <div v-else class="card-body">
       <b-row>
         <b-col cols="8"></b-col>
-        <b-col cols="4"> <b-button variant="primary float-right" v-b-modal.modal-1>Tạo mới</b-button> </b-col>
+        <b-col cols="4">
+          <b-button variant="primary float-right" v-b-modal.modal-1>Tạo mới</b-button>
+        </b-col>
       </b-row>
       <div class="row">
         <table class="table">
@@ -26,32 +28,84 @@
               <td>{{item.ProName}}</td>
               <td>{{item.Price}}</td>
               <td>{{item.TinyDes}}</td>
-              <td> 
+              <td>
                 <b-row>
-                  <b-col class="px-0"><b-button variant="success" size="sm"><BIconPencil/></b-button></b-col>
-                  <b-col class="px-0"><b-button variant="danger" size="sm"><BIconXCircle/></b-button></b-col>
+                  <b-col class="px-0">
+                    <b-button variant="success" size="sm">
+                      <BIconPencil />
+                    </b-button>
+                  </b-col>
+                  <b-col class="px-0">
+                    <b-button variant="danger" size="sm">
+                      <BIconXCircle />
+                    </b-button>
+                  </b-col>
                 </b-row>
               </td>
             </tr>
           </tbody>
         </table>
       </div>
-      <b-button v-if="hasMore" block variant="primary" @click="fetchProducts($route.params.catId,2)">Load more...</b-button>
+      <b-button
+        v-if="hasMore"
+        block
+        variant="primary"
+        @click="fetchProducts($route.params.catId,2)"
+      >Load more...</b-button>
     </div>
-    
-    <b-modal id="modal-1" :title="this.isEdit ? 'tạo mới' : '' " >
-      <p class="my-4"></p>
+
+    <b-modal size="lg" id="modal-1" :title="this.isEdit ? 'tạo mới' : '' ">
+      <b-row>
+        <b-col cols="4" class="mt-2">
+          <label for="ProName">ProName</label>
+          <b-input-group size="sm" prepend="ProName" append="">
+            <b-form-input></b-form-input>
+          </b-input-group>
+        </b-col>
+        <b-col cols="4" class="mt-2">
+          <label for="TinyDes">TinyDes</label>
+          <b-input-group size="sm" prepend="TinyDes" append="">
+            <b-form-input></b-form-input>
+          </b-input-group>
+        </b-col>
+        <b-col cols="4" class="mt-2">
+          <label for="FullDes">FullDes</label>
+          <b-input-group size="sm" prepend="FullDes" append="">
+            <b-form-input></b-form-input>
+          </b-input-group>
+        </b-col>
+        <b-col cols="4" class="mt-2">
+          <label for="Price">Price</label>
+          <b-input-group size="sm" prepend="Price" append="">
+            <b-form-input></b-form-input>
+          </b-input-group>
+        </b-col>
+        <b-col cols="4" class="mt-2">
+          <label for="Category">Category</label>
+          <b-input-group size="sm" prepend="Category" append="">
+            <b-form-input></b-form-input>
+          </b-input-group>
+        </b-col>
+        <b-col cols="4" class="mt-2">
+            <label>Quantity</label>
+          <b-input-group size="sm" prepend="Quantity" append="">
+            <b-form-input></b-form-input>
+          </b-input-group>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
     </b-modal>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import {  BIconPencil, BIconXCircle } from 'bootstrap-vue'
+import { BIconPencil, BIconXCircle } from "bootstrap-vue";
 
 export default {
   components: {
-    BIconPencil,BIconXCircle
+    BIconPencil,
+    BIconXCircle
   },
   data() {
     return {
@@ -81,7 +135,7 @@ export default {
       axios
         .get(`http://localhost:3000/categories/${catId}/products`)
         .then(res => {
-          console.log(res)
+          console.log(res);
           this.list = res.data;
           this.empty = this.list.length === 0;
         })
