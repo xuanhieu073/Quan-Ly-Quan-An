aadeaf7b60d6369be85f88a0ef25a5dea79fbfa2
xuanhieu073 <xuanhieu073@gmail.com>
2020-02-27 18:26:05 +0700

    lấy IdTinhThanh từ tinhthanhddl

--- front-end/src/views/Branch.vue
+++ front-end/src/views/Branch.vue
@@ -98,7 +98,7 @@ export default {
     //     })
     //     .catch(err => {
     //       console.error(err);
-    //     });
+    //     });ss
     // },
     test: function(){
       this.$store.commit('hideChiNhanhDetail')
